4f98b2e98b160bbec987f64c0c46fe062519b32e
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-11-04 16:24:21 -0600

    added get route for homepage and totalDuration parameters to not show as undefined

--- controllers/api/workoutRoute.js
+++ controllers/api/workoutRoute.js
@@ -1,2 +1,22 @@
 const router = require('express').Router();
-const { workout } = require('../../models');
+const Workout = require('../../models/workout');
+
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    }
+  ])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
