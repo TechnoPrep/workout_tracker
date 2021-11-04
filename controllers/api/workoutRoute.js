dc8718e6e61efb4fe8c6d727f15eb1a9040b5b43
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-11-04 16:48:50 -0600

    Added put route to add exercise to the workout array

--- controllers/api/workoutRoute.js
+++ controllers/api/workoutRoute.js
@@ -19,4 +19,52 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    }
+  ])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id,
+    {
+      $push: {
+        exercises: req.body
+      },
+    },
+    {
+      new: true,
+    }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
 module.exports = router;
