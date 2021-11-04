bac0f452d02714d0f70146838c5c8b3a10b7ef1e
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-11-04 15:13:46 -0600

    created model for the workout db

--- models/workout.js
+++ models/workout.js
@@ -3,21 +3,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for workout"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
   date: {
     type: Date,
     default: Date.now
-  }
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        require: "Enter the type for the workout"
+      },
+      name: {
+        type: String,
+        trim: true,
+        require: "Enter a name for the workout"
+      },
+      duration: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      }
+    }
+  ]
 });
 
-const workout = mongoose.model("workout", workoutSchema);
+const workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = workout;
